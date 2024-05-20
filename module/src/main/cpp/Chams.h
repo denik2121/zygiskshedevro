be6035f1a795248b14458d65c9e3a84ca713ad8f
denik2121 <87583925+denik2121@users.noreply.github.com>
2024-05-20 21:07:50 +1245

    Update Chams.h

--- module/src/main/cpp/Chams.h
+++ module/src/main/cpp/Chams.h
@@ -1,52 +1,26 @@
-#ifndef ZEFF_CHAMS
-#define ZEFF_CHAMS
 #include <GLES2/gl2.h>
-#include <GLES/gl.h>
 #include <dlfcn.h>
-#include <array>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <fstream>
-#include <iostream>
-using namespace std;
+#include <Substrate/SubstrateHook.h>
+#include <Substrate/CydiaSubstrate.h>
+
 static void *handle;
 static const char* shaderName;
+static bool enableWallhack;
+static bool enableWallhackW;
+static bool enableWallhackG;
+static bool enableWallhackO;
 static bool enableRainbow;
-static float r = 0.0f;
-static float g = 0.0f;
-static float b = 0.0f;
-static int w = 1;
+static bool enableRainbow1;
+static float r = 0;
+static float g = 0;
+static float b = 0;
+static int w = 0;
+static int a = 0;
 
-float red = 255.0f;
-float gren = 0.0f;
-float blue = 0.0f;
-float mi = 0.0f;
-
-int chamsint = 0;
-
-//ZEFF: For ImGui ColorEdit or ColorPicker
-ImVec4 chamsColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-ImVec4 wallColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-ImVec4 visibleColor = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-
-//ZEFF: For ImGui Combo or something else
-const char* Chams[] = {"OFF", "Default", "Shading", "Wireframe", "Outline", "Visibility Check Outline", "Visibility Check Shading", "Visibility Check Shading + Outline"};
-
-const char *isDumpLocation = ("/sdcard/Download/Shaders.txt");
-
-//ZEFF: Function with shader name dumping
-string isDump(const char *name)
-{
-    vector<string> isShaders = {name};
-
-    static string isTotalShaders;
-
-    for (const auto &isAddTo: isShaders)
-        isTotalShaders += (isAddTo + "\n"); // Adding whatever is added to the string vector to isTotalShaders, with a new line added per shader.
-
-    return isTotalShaders.c_str();
-}
+float red = 0;
+float gren = 0;
+float blue =0;
+float mi = 0;
 
 void setShader(const char* s) {
     shaderName = s;
@@ -55,188 +29,196 @@ const char* getShader() {
     return shaderName;
 }
 
-void SetR(float set){
+void SetWallhack(bool enable){
+    enableWallhack = enable;
+}
+
+void SetWallhackW(bool enable){
+    enableWallhackW = enable;
+}
+
+void SetWallhackG(bool enable){
+    enableWallhackG = enable;
+}
+
+void SetWallhackO(bool enable){
+    enableWallhackO = enable;
+}
+
+void SetRainbow(bool enable){
+    enableRainbow = enable;
+}
+void SetRainbow1(bool enable){
+    enableRainbow1 = enable;
+}
+
+void SetR(int set){
     r = set;
 }
 
-void SetG(float set){
+void SetG(int set){
     g = set;
 }
 
-void SetB(float set){
+void SetB(int set){
     b = set;
 }
 
-//ZEFF: Uncomment the code if you want a shader dump. Don't leave the code uncommented because it causes lag during the game.
+void SetW(int set){
+    w = set;
+}
+
+bool getWallhackEnabled(){
+    return enableWallhack;
+}
+
+bool getWireframeEnabled(){
+    return enableWallhackW;
+}
+
+bool getGlowEnabled(){
+    return enableWallhackG;
+}
+
+bool getOutlineEnabled(){
+    return enableWallhackO;
+}
+
+bool getRainbowEnabled(){
+    return enableRainbow;
+}
+bool getRainbow1Enabled(){
+    return enableRainbow1;
+}
+
 int (*old_glGetUniformLocation)(GLuint, const GLchar *);
 GLint new_glGetUniformLocation(GLuint program, const GLchar *name) {
-    //ofstream(isDumpLocation) << isDump(name);
+
     return old_glGetUniformLocation(program, name);
+}
+
+bool isCurrentShader(const char *shader) {
+    GLint currProgram;
+    glGetIntegerv(GL_CURRENT_PROGRAM, &currProgram);
+    return old_glGetUniformLocation(currProgram, shader) != -1;
 }
 
 void (*old_glDrawElements)(GLenum mode, GLsizei count, GLenum type, const void *indices);
 void new_glDrawElements(GLenum mode, GLsizei count, GLenum type, const void *indices) {
-    //ZEFF: Old code
-    //if (mode != GL_LINES && count < 1000) return old_glDrawElements(mode, count, type, indices);
+    old_glDrawElements(mode, count, type, indices);
+    if (mode != GL_TRIANGLES || count < 1000) return;
+    {
+        GLint currProgram;
+        glGetIntegerv(GL_CURRENT_PROGRAM, &currProgram);
 
-    //ZEFF: New code with object index counts
-    if (mode != GL_LINES && count != 786 && count != 852) return old_glDrawElements(mode, count, type, indices);
+        GLint id = old_glGetUniformLocation(currProgram, getShader());
+        if (id == -1) return;
 
-    GLint currProgram;
-    glGetIntegerv(GL_CURRENT_PROGRAM, &currProgram);
-
-    GLint id = old_glGetUniformLocation(currProgram, getShader());
-    if (id == -1) return old_glDrawElements(mode, count, type, indices);
-    
-    GLboolean blend;
-    glGetBooleanv(GL_BLEND, &blend);
-    if (blend) return old_glDrawElements(mode, count, type, indices);
-
-    GLboolean depthTest;
-    glGetBooleanv(GL_DEPTH_TEST, &depthTest);
-    if (!depthTest) return old_glDrawElements(mode, count, type, indices);
-
-    //OFF
-    if (chamsint == 0) {
-        old_glDrawElements(mode, count, type, indices);
-    }
-
-    //Default Chams
-    if (chamsint == 1) {
-        glDepthRangef(1, 0.5);
-        old_glDrawElements(GL_TRIANGLES, count, type, indices);
-    }
-
-    //Shading Chams
-    if (chamsint == 2) {
-        glEnable(GL_BLEND);
-        glDepthRangef(1, 0.5);
-        glBlendFunc(GL_ONE, GL_CONSTANT_COLOR);
-        glBlendColor(GLfloat(r), GLfloat(g), GLfloat(b), 1);
-        old_glDrawElements(GL_TRIANGLES, count, type, indices);
-    }
-
-    //Wireframe Chams
-    if (chamsint == 3) {
-        old_glDrawElements(mode, count, type, indices);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_CONSTANT_COLOR, GL_CONSTANT_ALPHA);
-        glDepthRangef(1, 0.5);
-        glBlendColor(GLfloat(r), GLfloat(g), GLfloat(b), 1);
-        glLineWidth(w);
-        old_glDrawElements(GL_LINES, count, type, indices);
-    }
-
-    //Outline Chams
-    if (chamsint == 4) {
-        old_glDrawElements(mode, count, type, indices);
-        glDepthRangef(1, 0);
-        glLineWidth(10);
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_CONSTANT_COLOR, GL_CONSTANT_ALPHA);
-        glBlendColor(0, 0, 0, 1);
-        old_glDrawElements(GL_TRIANGLES, count, type, indices);
-        glDepthRangef(1, 0.5);
-        glBlendColor(GLfloat(r), GLfloat(g), GLfloat(b), 1);
-        old_glDrawElements(GL_LINES, count, type, indices);
-    }
-
-    //Visible Check Outline Chams
-    if (chamsint == 5) {
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_CONSTANT_COLOR, GL_CONSTANT_ALPHA);
-        glLineWidth(10);
-        glBlendColor(GLfloat(r), GLfloat(g), GLfloat(b), 1);
-        glDepthRangef(0.5, 1);
-        old_glDrawElements(GL_LINES, count, type, indices);
-        glBlendFunc(GL_ONE_MINUS_CONSTANT_COLOR, GL_ONE_MINUS_CONSTANT_ALPHA);
-        glBlendColor(0, 0, 0, 1);
-        glDepthRangef(1, 0.5);
-        old_glDrawElements(GL_TRIANGLES, count, type, indices);
-    }
-
-    //ZEFF: If you have java source, replace glBlendColor parameters
-    //Visible Check Shading Chams
-    if (chamsint == 6) {
-        glEnable(GL_BLEND);
-        glDisable(GL_DEPTH_TEST);
-        glBlendFunc(GL_ONE, GL_CONSTANT_COLOR);
-        glBlendColor(wallColor.x, wallColor.y, wallColor.z, 1);
-        old_glDrawElements(GL_TRIANGLES, count, type, indices);
-        glEnable(GL_DEPTH_TEST);
-        glDepthRangef(0.5, 1);
-        glBlendColor(visibleColor.x, visibleColor.y, visibleColor.z, 1);
-        old_glDrawElements(GL_TRIANGLES, count, type, indices);
-    }
-
-    //Visible Check Shading + Outline Chams
-    if (chamsint == 7) {
-        glEnable(GL_BLEND);
-        glDisable(GL_DEPTH_TEST);
-        glBlendFunc(GL_ONE, GL_CONSTANT_COLOR);
-        glBlendColor(wallColor.x, wallColor.y, wallColor.z, 1);
-        old_glDrawElements(GL_TRIANGLES, count, type, indices);
-        glEnable(GL_DEPTH_TEST);
-        glBlendFunc(GL_CONSTANT_COLOR, GL_CONSTANT_ALPHA);
-        glDepthRangef(0.5, 1);
-        glBlendColor(visibleColor.x, visibleColor.y, visibleColor.z, 1);
-        glLineWidth(10);
-        old_glDrawElements(GL_LINES, count, type, indices);
-        glBlendFunc(GL_ONE_MINUS_CONSTANT_COLOR, GL_ONE_MINUS_CONSTANT_ALPHA);
-        glDepthRangef(0.49, 1);
-        glBlendColor(0, 0, 0, 1);
-        old_glDrawElements(GL_TRIANGLES, count, type, indices);
-    }
-
-    if (enableRainbow && chamsint != 6 && chamsint != 7) {
-        if (red == 255) {
-            if (blue == 0) {
-                if (gren == 255) {}
-                else {
-                    gren = gren + 1;
-                }
+        if (getWireframeEnabled()) {
+			
+            if (enableWallhackW) {
+                glDepthRangef(1, 0.5);
             }
-        }
-        if (gren == 255) {
-            if (red == 0) {}
             else {
-                red = red - 1;
+                glDepthRangef(0.5, 1);
             }
+            glBlendColor(GLfloat(r/255), GLfloat(g/255), GLfloat(b/255), 1);
+            glColorMask(1, 1, 1, 1);
+            glEnable(GL_BLEND);
+            glBlendFuncSeparate(GL_CONSTANT_COLOR, GL_CONSTANT_ALPHA, GL_ONE, GL_ZERO);
+            glLineWidth(w);
+            old_glDrawElements(GL_LINE_LOOP, count, type, indices);
         }
-        if (gren == 255) {
-            if (red == 0) {
-                if (blue == 255) {}
-                else {
-                    blue = blue + 1;
-                }
+
+
+        if (getWallhackEnabled()) {
+			glBlendColor(GLfloat(r/255), GLfloat(g/255), GLfloat(b/255), GLfloat(a/255));
+            glColorMask(r, g, b, 255);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_CONSTANT_ALPHA, GL_CONSTANT_COLOR);
+        }
+
+        if (getGlowEnabled()) {
+            glEnable(GL_BLEND);
+            glBlendColor(GLfloat(r/255), GLfloat(g/255), GLfloat(b/255), 1);
+            glColorMask(1, 1, 1, 1);
+            glEnable(GL_BLEND);
+            glBlendFuncSeparate(GL_CONSTANT_COLOR, GL_CONSTANT_ALPHA, GL_ONE, GL_ZERO);
+            glLineWidth(15);
+   
+            glDepthRangef(0.5, 1);
+            old_glDrawElements(GL_LINES, count, type, indices);
+            glBlendColor(1, 1, 1, 1);
+            glDepthRangef(1, 0.5);
+            old_glDrawElements(GL_TRIANGLES, count, type, indices);
+        }
+		
+        if (getOutlineEnabled()) {
+            glDepthRangef(1, 0.5);
+            glLineWidth(20.0f);
+            glEnable(GL_BLEND);
+            glColorMask(1, 1, 1, 1);
+            glBlendFuncSeparate(GL_CONSTANT_COLOR, GL_CONSTANT_ALPHA, GL_ONE, GL_ZERO);
+            glBlendColor(0, 0, 0, 1);
+            old_glDrawElements(GL_TRIANGLES, count, type, indices);
+            glBlendColor(GLfloat(r/255), GLfloat(g/255), GLfloat(b/255), 1);
+            old_glDrawElements(GL_LINES, count, type, indices);
+        }
+		
+        if (getRainbowEnabled()) {
+	        if(getRainbow1Enabled){
+               if (red == 255){
+                   if (blue == 0 ){
+                       if (gren == 255){} else{
+                           gren = gren+1;
+                       }
+                   }
+               }
+               if (gren == 255){
+                   if (red == 0){} else{
+                       red = red-1;
+                   }
+               }
+               if (gren == 255) {
+                   if (red == 0) {
+                       if (blue==255){} else{
+                           blue = blue+1;
+                       }
+                   }
+               }
+               if (blue == 255) {
+                   if (gren == 0) {
+                       mi = 1;
+                       red = red+1;
+                   } 
+				   else{
+                        gren = gren-1;
+                   }
+               }
+               if (mi == 1){
+                   if (red == 255){
+                       if (blue == 0){} else{
+                           blue = blue-1;
+                       }
+                   }
+               }
+               SetR(red);
+               SetG(gren);
+               SetB(blue);
             }
+            glBlendColor(GLfloat(r/255), GLfloat(g/255), GLfloat(b/255), GLfloat(a/255));
+            glColorMask(1, 1, 1, 1);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_ONE_MINUS_CONSTANT_COLOR, GL_ONE_MINUS_CONSTANT_ALPHA);
+            
         }
-        if (blue == 255) {
-            if (gren == 0) {
-                mi = 1;
-                red = red + 1;
-            } else {
-                gren = gren - 1;
-            }
-        }
-        if (mi == 1) {
-            if (red == 255) {
-                if (blue == 0) {}
-                else {
-                    blue = blue - 1;
-                }
-            }
-        }
-        SetR(red / 255);
-        SetG(gren / 255);
-        SetB(blue / 255);
+        old_glDrawElements(mode, count, type, indices);
+
+        glDepthRangef(0.5, 1);
+
+        glColorMask(1, 1, 1, 1);
+        glDisable(GL_BLEND);
     }
-    //ZEFF: Return to original drawing settings
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glBlendColor(0, 0, 0, 1);
-    glLineWidth(1);
-    glDepthRangef(0.5, 1);
-    glDisable(GL_BLEND);
 }
 
 bool mlovinit(){
@@ -254,7 +236,7 @@ void LogShaders(){
     if(dlsym_error){
         return;
     }else{
-        DobbyHook(reinterpret_cast<void*>(p_glGetUniformLocation), reinterpret_cast<void*>(new_glGetUniformLocation), reinterpret_cast<void**>(&old_glGetUniformLocation));
+        MSHookFunction(reinterpret_cast<void*>(p_glGetUniformLocation), reinterpret_cast<void*>(new_glGetUniformLocation), reinterpret_cast<void**>(&old_glGetUniformLocation));
     }
 }
 
@@ -264,8 +246,6 @@ void Wallhack(){
     if(dlsym_error){
         return;
     }else{
-        DobbyHook(reinterpret_cast<void*>(p_glDrawElements), reinterpret_cast<void*>(new_glDrawElements), reinterpret_cast<void**>(&old_glDrawElements));
+        MSHookFunction(reinterpret_cast<void*>(p_glDrawElements), reinterpret_cast<void*>(new_glDrawElements), reinterpret_cast<void**>(&old_glDrawElements));
     }
 }
-
-#endif
